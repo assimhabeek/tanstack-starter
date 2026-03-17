2e742eaedbc515e6eac1974ab8ca08583de4d532
assim habeek <assimhabe@gmail.com>
2026-03-17 01:28:18 +0100

    ci(build.tf): disable combine artifacts for batch builds

--- infrastructure/terraform/build.tf
+++ infrastructure/terraform/build.tf
@@ -87,8 +87,9 @@ resource "aws_codebuild_project" "codebuild" {
   }
 
   build_batch_config {
-    service_role    = aws_iam_role.codebuild_pr_role.arn
-    timeout_in_mins = 60
+    service_role      = aws_iam_role.codebuild_pr_role.arn
+    timeout_in_mins   = 60
+    combine_artifacts = false
   }
 }
 
