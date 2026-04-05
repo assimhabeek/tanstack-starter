2f7cda652b27309f396175d5c2bb64789e0e1173
assim habeek <assimhabe@gmail.com>
2026-04-05 22:06:30 +0100

    ci: testing mock infra

--- infrastructure/temp/main.tf
+++ infrastructure/temp/main.tf
@@ -12,5 +12,11 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "name" {
+  bucket        = "my-easy-delete-bucket-123456"
+  force_destroy = true
 }
